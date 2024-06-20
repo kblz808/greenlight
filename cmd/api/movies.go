fac8355420345878ac4217f3bb891505d76a6e60
kaleab <808kaleabkzin@gmail.com>
2024-06-20 10:28:03 +0300

    sending error messages

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -14,7 +14,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -29,8 +29,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Print(err)
-		http.Error(w, "internal server error", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 
 	fmt.Fprintf(w, "show the detals of movie %d\n", id)
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,9 +8,13 @@ import (
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	return router
 }
