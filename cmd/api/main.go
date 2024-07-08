7b520fdbfcfb58080d5699b1c207b108a6ea5253
kaleab <808kaleabkzin@gmail.com>
2024-07-08 11:49:02 +0300

    creating custom metrics

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
+	"runtime"
 
 	"flag"
 	"greenlight/internal/data"
@@ -90,6 +92,20 @@ func main() {
 	defer db.Close()
 
 	logger.PrintInfo("database connection pool established", nil)
+
+	expvar.NewString("version").Set(version)
+
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+
+	expvar.Publish("timestamp", expvar.Func(func() any {
+		return time.Now().Unix()
+	}))
 
 	app := &application{
 		config: cfg,
